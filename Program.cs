9b3410bbf34a3dccf293fe8172ac769c937ba3b5
Aki_san <rikale.aw@gmail.com>
2020-12-05 21:12:05 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestApp_Solar_TaskManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
